d13e37ac7f633b99c8a93154d36bd411762c9b70
Raiyaan Tarique <133511286+RynTrq@users.noreply.github.com>
2026-04-13 06:03:37 +0530

    Time: 52 ms (41%), Space: 90.8 MB (59.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        
+        unordered_set<int> mySet;
+
+        for(int i=0; i<n; i++){
+            if(mySet.find(nums[i]) == mySet.end()){
+                mySet.insert(nums[i]);
+            }else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
